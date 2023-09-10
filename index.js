d26675cdbf60692818abdeeb6eb70aea5801354f
lwalker <landen@example.com>
2023-09-09 19:37:56 -0500

    created connection to database + inquirer prompt in index.js

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-const database = require('./db/schema.sql');
-const data = require('./db/seeds.sql');
+const mysql = require('mysql2');
 
-
-inquirer.prompt([
+db = mysql.createConnection(
     {
-        type: 'list',
-        name: 'database',
-        message: 'What would you like to view?',
-        choices: ['View all departments', 'View all roles', 'View all employees',
-    'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+    host: 'localhost',
+    user:'root',
+    password: '',
+    datqabase: 'employee_db',
     },
-]).then((inquirerResponses) => {
-    let database;
-    if (inquirerResponses.database === 'circle') {
-        database = new Circle(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
-    }
+    console.log('connected to employee db')
+);
 
-    if (inquirerResponses.database === 'triangle') {
-        database = new Triangle(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
-    }
-
-    if (inquirerResponses.database === 'square') {
-        database = new Square(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
-    }
-
-    shape.setShape(shape.render()) 
-    let SVG = shape.renderSVG()
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+            { choice: 'View all departments', value: 'ALL_DEPARTMENTS' },
+            { choice: 'View all roles', value: 'ALL_ROLES' },
+            { choice: 'View all employees', value: 'ALL_EMPLOYEES' },
+            { choice: 'Add a department', value: 'ADD_DEPARTMENT' },
+            { choice: 'Add an employee role', value: 'ADD_ROLE' },
+            { choice: 'Add an employee', value: 'ADD_EMPLOYEE' },
+            { choice: 'Update an employee role', value: 'UPDATE_EMPLOYEE' },
+        },
+    ]).then((inquirerResponses) => {
+        let database;
+        if (inquirerResponses.value === 'ALL_DEPARTMENTS') {
+            database = new Circle(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
+        }
     
-    fs.writeFileSync("./logo.svg", SVG)
-    console.log("Generated Logo.svg")
-})
+        if (inquirerResponses.database === 'triangle') {
+            database = new Triangle(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
+        }
+    
+        if (inquirerResponses.database === 'square') {
+            database = new Square(inquirerResponses.shapeColor, inquirerResponses.brandInitials, inquirerResponses.textColor)
+        }
+    
+        shape.setShape(shape.render()) 
+        let SVG = shape.renderSVG()
+        
+        fs.writeFileSync("./logo.svg", SVG)
+        console.log("Generated Logo.svg")
+    })
+}
